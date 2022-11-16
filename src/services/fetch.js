3bdf8e37e64c9836e9e334c1d5b4bbc308abb5fe
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2022-11-16 20:54:26 +0100

    update fetch

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -27,9 +27,9 @@ export async function fetchMovieByID(id) {
 }
 
 
-export async function fetchMoviesByQuery(query, page) {
+export async function fetchMoviesByQuery(query) {
   const response = await axios.get(
-    `/${params.search}?api_key=${api_key}&query=${query}&language=en-US&page=${page}&include_adult=false`
+    `/${params.search}?api_key=${api_key}&query=${query}&language=en-US&include_adult=false`
   );
   return response.data;
 }
